f7b57d475b52f9e3ab8dd413dc2428bf67d1ac4e
Thiago Uriel <thiago.uriel@gmail.com>
2025-10-18 17:26:24 -0700

    Added scripts for NBS dependencies.

---
+++ Scripts/initialize_dependencies.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+echo === Cloning Ninja Bear Studio plugins ===
+
+:: Set the Plugins folder path relative to this script
+set "PLUGIN_DIR=%~dp0..\Plugins\NinjaBearStudio"
+
+:: Create the folder if it doesn't exist
+if not exist "%PLUGIN_DIR%" (
+    mkdir "%PLUGIN_DIR%"
+)
+
+cd /d "%PLUGIN_DIR%"
+
+:: List of primary repositories
+set REPO1=https://github.com/NinjaBearStudioTech/NinjaCombat.git
+set REPO2=https://github.com/NinjaBearStudioTech/NinjaCommonUI.git
+set REPO3=https://github.com/NinjaBearStudioTech/NinjaGAS.git
+set REPO4=https://github.com/NinjaBearStudioTech/NinjaFactions.git
+set REPO5=https://github.com/NinjaBearStudioTech/NinjaInput.git
+set REPO6=https://github.com/NinjaBearStudioTech/NinjaInteraction.git
+set REPO7=https://github.com/NinjaBearStudioTech/NinjaInventory.git
+set REPO8=https://github.com/NinjaBearStudioTech/NinjaPerception.git
+set REPO9=https://github.com/NinjaBearStudioTech/NinjaProfile.git
+set REPO10=https://github.com/NinjaBearStudioTech/NinjaTokens.git
+
+for %%R in (1 2 3 4 5 6 7 8 9 10) do (
+    call set "REPO=!REPO%%R!"
+    for %%A in (!REPO!) do (
+        set "REPO_URL=%%A"
+        for %%F in (!REPO_URL!) do set "FOLDER=%%~nF"
+
+        if not exist "!FOLDER!" (
+            echo Cloning !REPO_URL! into !FOLDER!...
+            git clone !REPO_URL!
+        ) else (
+            echo Folder !FOLDER! already exists. Skipping.
+        )
+    )
+)
+
+echo === Plugin initialization complete ===
+endlocal
---
+++ Scripts/update_plugins.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+echo === Refreshing Ninja Bear Studio plugins ===
+
+REM Set the Plugins folder path relative to this script
+set "PLUGIN_DIR=%~dp0..\Plugins\NinjaBearStudio"
+
+REM List of plugin folder names (must match cloned repo folders)
+set PLUGINS=NinjaCombat NinjaCommonUI, NinjaGAS NinjaFactions NinjaInput NinjaInteraction NinjaInventory NinjaProfile NinjaPerception NinjaTokens
+
+REM Change to plugin directory
+cd /d "%PLUGIN_DIR%"
+
+for %%P in (%PLUGINS%) do (
+    if exist "%%P\.git" (
+        echo Updating %%P...
+        pushd %%P >nul
+
+        git pull
+        if !errorlevel! neq 0 (
+            echo [ERROR] Failed to update %%P
+        )
+
+        popd >nul
+    ) else (
+        echo Skipping %%P - not a valid git repo
+    )
+)
+
+echo === Plugin refresh complete ===
+endlocal
